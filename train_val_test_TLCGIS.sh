759c9812f926eff2973db996e9e4f5e387f4132c
liulingbo918 <liulingbo918@gmail.com>
2022-08-24 21:51:08 +0800

    Update train_val_test_TLCGIS.sh

--- train_val_test_TLCGIS.sh
+++ train_val_test_TLCGIS.sh
@@ -4,9 +4,9 @@ python main.py \
         --gpu_ids 1,2 \
         --epochs 50 \
         --dataset "TLCGIS" \
-	      --split_train_val_test "dataset/fusion_lidar_images_sigspatial18/" \
-	      --sat_dir "dataset/fusion_lidar_images_sigspatial18/rgb/" \
-	      --mask_dir "dataset/fusion_lidar_images_sigspatial18/mask/" \
-	      --lidar_dir "dataset/fusion_lidar_images_sigspatial18/depth_lpu/" \
+	--split_train_val_test "dataset/fusion_lidar_images_sigspatial18/" \
+	--sat_dir "dataset/fusion_lidar_images_sigspatial18/rgb/" \
+	--mask_dir "dataset/fusion_lidar_images_sigspatial18/mask/" \
+	--lidar_dir "dataset/fusion_lidar_images_sigspatial18/depth_lpu/" \
         --weight_save_dir 'save_model/' \
         --lr 1e-4  
